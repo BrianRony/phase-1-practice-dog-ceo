fba52302f79e7e60ce9a8900ae9e3abcd55e46b2
Brian Ngetich <kipronobrian97@gmail.com>
2024-04-16 00:30:28 +0300

    Added random dogs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", () => {
+function addDog() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+      .then(res=> res.json())
+      .then(results => {
+        results.message.forEach(image => addImage(image))
+      });
+  }
+  
+  function addImage(dogPic) {
+    let imageContainer = document.querySelector('#dog-image-container');
+    let image = document.createElement('img');
+    image.src = dogPic;
+    imageContainer.appendChild(image);
+  }
+  
+
+function init(){
+addDog()
+}
+init()
+})
